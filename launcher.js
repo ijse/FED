7e3f936a9b71eb48635b9b719648d4a36926947f
ijse <i@ijser.cn>
2013-01-10 20:33:31 +0800

    修改配置文件位置，存入config/文件夹下

--- launcher.js
+++ launcher.js
@@ -4,23 +4,20 @@
  * @author ijse
  */
 //TODO: Auto reload when update codes
-//TODO: Auto generate doc
 //TODO: Deploy to server by ftp/sftp
 //TODO: Html export
 //TODO: Some more utils: jsCompressor, file utils...
 //TODO: Plugins support
 //TODO: Test suits
 
-//TODO: Use ./config.json as config file
-
-var VERSION = "0.0.3";
+var VERSION = require("./package.json").version;
 var commander = require("commander");
 var path = require("path");
 
 var proxyServer = require("./proxyServer");
 var localServer = require("./localServer");
 
-var gConfig = require("./globalConfig");
+var gConfig = require("./configs");
 
 // Add commander support
 commander
