3fa7282bbfc1c5473649e54dca0e3ef8dbbf6782
ijse <i@ijser.cn>
2013-02-04 15:07:46 +0800

    update fedutil

--- libs/fedUtil.js
+++ libs/fedUtil.js
@@ -5,31 +5,35 @@
  */
 var path = require("path");
 
-// Convert Path Object
-// ===================
-// convert path to normal style.
-// @param pathObj ={ item: path, ...}
-// @return pathObj
-exports.convPath = function(basePath, pathObj) {
-	for (var i in pathObj) {
-		var p = pathObj[i];
-		p = p[0] == "." ? path.join(basePath, p) : path.normalize(p);
-		pathObj[i] = p;
-	}
-	return pathObj;
-};
+module.exports = {
+	// Convert Path Object
+	// ===================
+	// convert path to normal style.
+	// @param pathObj ={ item: path, ...}
+	// @return pathObj
+	convPath: function(basePath, pathObj) {
+		for (var i in pathObj) {
+			var p = pathObj[i];
+			// p = p[0] == "." ? path.join(basePath, p) : path.normalize(p);
+			p = this.realPath(basePath, p);
+			pathObj[i] = p;
+		}
+		return pathObj;
+	},
 
-// Format the path
-// ===============
-// d:/aaa.txt ==> d:\\aaa.txt
-// ./bb.txt ==> {curPath}\\bb.txt
-// cc.txt ==> {curPath}\\cc.txt
-exports.realPath = function(base, p) {
-	var r = "";
-	if(p[1] === ":" || p[0] === "/") {
-		r = path.normalize(p);
-	} else {
-		r = path.join(base, p);
+	// Format the path
+	// ===============
+	// d:/aaa.txt ==> d:\\aaa.txt
+	// ./bb.txt ==> {curPath}\\bb.txt
+	// cc.txt ==> {curPath}\\cc.txt
+	realPath: function(base, p) {
+		var r = "";
+		if(p[1] === ":" || p[0] === "/") {
+			r = path.normalize(p);
+		} else {
+			r = path.join(base, p);
+		}
+		return r;
 	}
-	return r;
-};
+}
+
