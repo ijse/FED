6e9bacbfc488c4b1b6a3e75719a26189d5f19072
ijse <i@ijser.cn>
2015-04-08 12:16:41 +0800

    fix test

--- test/test.js
+++ test/test.js
@@ -9,26 +9,34 @@ var exec = function(cmd, cb) {
 }
 var spawn = require('child_process').spawn;
 
-fed = [ 'node', fed, ' '].join(' ');
+var fedbin = [ 'node', fed, ' '].join(' ');
+
+var kill = function(pid) {
+  if(process.platform === 'win32') {
+    exec('taskkill /f /t /pid ' + pid);
+  } else {
+    exec('kill /f ' + pid);
+  }
+};
 
 describe('Test fed showing help message', function() {
 
   it('show help message when -h', function(done) {
-    exec(fed + '-h', function(err, so, se) {
+    exec(fedbin + '-h', function(err, so, se) {
       so.should.match(/Usage: fed \[options\] \[command\]/);
       done(err);
     });
   });
 
   it('show help message when help', function(done) {
-    exec(fed + 'help', function(err, so, se) {
+    exec(fedbin + 'help', function(err, so, se) {
       so.should.match(/Usage: fed \[options\] \[command\]/);
       done(err);
     });
   });
 
   it('show sub command help message', function(done) {
-    exec(fed + 'help server', function(err, so, se) {
+    exec(fedbin + 'help server', function(err, so, se) {
       so.should.match(/Usage: fed-server \[options\] \[dir\]/);
       done(err);
     });
@@ -42,21 +50,21 @@ describe('Test fed showing versions', function() {
   var serVer = require('../node_modules/serve2/package').version
 
   it('show fed version', function(done) {
-    exec(fed + 'version', function(e, so, se) {
+    exec(fedbin + 'version', function(e, so, se) {
       so.should.match(new RegExp(fedVer));
       done(e);
     });
   });
 
   it('show serve2 version', function(done) {
-    exec(fed + 'server version', function(e, so, se) {
+    exec(fedbin + 'server version', function(e, so, se) {
       so.should.match(new RegExp(serVer));
       done(e);
     });
   });
 
   it('show fed and serve2 versions', function(done) {
-    exec(fed + 'versions', function(e, so, se) {
+    exec(fedbin + 'versions', function(e, so, se) {
       so.should.match(new RegExp(fedVer));
       so.should.match(new RegExp(serVer));
       done(e);
@@ -70,39 +78,58 @@ describe('Test fed starting server', function() {
   this.timeout(5000);
 
   var p = null;
-  afterEach(function() {
-    p.kill();
-  });
   it('start static server default current workdir', function(done) {
-    //var p = exec(fed + 'server -p 3000 .');
-    p = spawn('node', [fed, 'server', '-p', '3000', '.']);
-    var request = Request('http://localhost:3000');
-    setTimeout(function() {
+    p = spawn('node', [fed, 'server', '-p', '3001', '.'], {timeout: 5000});
+    p.stderr.on('data', function(e) {
+      console.log('error', ''+e);
+      done(e);
+    });
+    var flag = false;
+    p.stdout.on('data', function(d) {
+      if(flag) return ;
+      var request = Request('http://localhost:3001');
       request
         .get('/package.json')
         .expect(200)
         .expect(/fed/)
         .expect(/version/)
         .end(function() {
-          done();
+          kill(p.pid);
         });
-    }, 1500);
+      flag = true;
+    });
+
+    p.on('exit', function() {
+      done();
+    });
+
   });
 
   it('start server with freemarker suport', function(done) {
-    //var p = exec(fed + 'server -p 3000 -M mock --view-root view ./test/res');
-    p = spawn('node', [fed, 'server', '-p', '3000', '-M', 'mock', '--view-root', 'view', './test/res']);
-    var request = Request('http://localhost:3000');
-    setTimeout(function() {
+    p = spawn('node', [fed, 'server', '-p', '3002', '-M', 'mock', '--view-root', 'view', './test/res'], {timeout: 5000});
+    p.stderr.on('data', function(e) {
+      console.log('error', ''+e);
+      done(e);
+    });
+    var flag = false;
+    p.stdout.on('data', function() {
+      if(flag) return ;
+      var request = Request('http://localhost:3002');
       request
         .get('/test.js')
         .expect(200)
         .expect(/hello/)
         .expect(/inner/)
         .end(function() {
-          done();
+          kill(p.pid);
         });
-    }, 1800);
+      flag = true;
+    });
+
+    p.on('exit', function() {
+      done();
+    });
+
   });
 
 });
